5a30bed0e0fda076a9a0b589a2ebec9b339e31fe
DOMENICO.DEGAETANO <d.degaetano5@studenti.uniba.it>
2021-10-18 17:14:34 +0200

    Added LaboratorioRepository and moved databaseinit class in config package

--- src/main/java/com/uniba/di/dfmdevelop/labservice/repository/LaboratorioRepository.java
+++ src/main/java/com/uniba/di/dfmdevelop/labservice/repository/LaboratorioRepository.java
@@ -2,10 +2,8 @@ package com.uniba.di.dfmdevelop.labservice.repository;
 
 import com.uniba.di.dfmdevelop.labservice.model.laboratorio.Laboratorio;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Optional;
 
 @Repository
@@ -13,8 +11,5 @@ public interface LaboratorioRepository extends JpaRepository<Laboratorio, Long> 
 
     Optional<Laboratorio> findById(Long id);
 
-    @Transactional
-    @Modifying
-    @Query("INSERT INTO laboratorio(codice_iban, indirizzo_stradale, nome_laboratorio, partita_iva, numero_telefono) VALUES ( ?, ?, ?, ?, ?)")
-    void addLaboratorio(Laboratorio laboratorio);
+    Laboratorio save(final Laboratorio laboratorio);
 }
