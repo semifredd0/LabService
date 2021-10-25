abd774a383624d68de583c6a52c0a4f0d57d7df0
FrancescoBrescia <brescia.francesco.fb.2000@gmail.com>
2021-10-26 00:02:57 +0200

    sviluppo della pagina home per il laboratorio
    Nota: ancora da terminare

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,9 +1,7 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginRight = "250px";
+    document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginRight= "0";
+    document.getElementById("mySidenav").style.width = "0";
 }
