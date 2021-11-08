d8d63df538df4bb6aa2baffe07297452775d6260
DOMENICO.DEGAETANO <d.degaetano5@studenti.uniba.it>
2021-11-08 11:58:16 +0100

    Completed test for ConfirmationTokenServiceTest but still 2 test to fix

--- src/test/java/com/uniba/di/dfmdevelop/labservice/service/ConfirmationTokenServiceTest.java
+++ src/test/java/com/uniba/di/dfmdevelop/labservice/service/ConfirmationTokenServiceTest.java
@@ -16,8 +16,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @Slf4j
 @ExtendWith(MockitoExtension.class)
@@ -34,21 +33,21 @@ public class ConfirmationTokenServiceTest {
     @Test
     public void saveConfirmationTokenWhenRegisteringOrLoggingIn() {
         log.info("Starting savingToken test");
-        // Dati per utente di prova
-        String email = "labo@prova.com";
-        String password = "prova";
-        String role = "Laboratorio";
-
-        // Dati per token di prova
-        String tokenString = "tokenprova";
-        LocalDateTime generatedAt = LocalDateTime.now();
-        LocalDateTime expiresAt = LocalDateTime.of(2021,11,5,18,30);
 
         log.info("Creazione utente prova per token di prova");
-        UtenteGenerico utenteGenerico = new UtenteGenerico(email,password,role);
+        UtenteGenerico utenteGenerico = new UtenteGenerico();
+        utenteGenerico.setId(1L);
+        utenteGenerico.setEmail("lab_lecce@labservice.it");
+        utenteGenerico.setPassword("prova");
+        utenteGenerico.setRole("LABORATORIO");
 
         log.info("Creazione token di prova");
-        ConfirmationToken token = new ConfirmationToken(tokenString,generatedAt,expiresAt,utenteGenerico);//EX 2017-01-13T17:09:42.411
+        ConfirmationToken token = new ConfirmationToken();//EX 2017-01-13T17:09:42.411
+        token.setId(1L);
+        token.setToken("prova");
+        token.setCreatedAt(LocalDateTime.now());
+        token.setExpiresAt(LocalDateTime.of(2021,11,5,18,30));
+        token.setUtenteGenerico(utenteGenerico);
 
         Mockito.when(confirmationTokenRepository.getById(token.getId())).thenReturn(token);
 
@@ -75,7 +74,34 @@ public class ConfirmationTokenServiceTest {
         log.info("Getting token");
         Optional<ConfirmationToken> tokenResult = confirmationTokenService.getToken(token);
 
-        assertTrue("token not found", tokenResult.isPresent());
+        assertFalse(tokenResult.isEmpty());
         log.info("Ending gettingToken test");
+    }
+
+    @Test
+    public void setConfirmationDateTimeOfToken(){
+
+        log.info("Creazione utente prova per token di prova");
+        UtenteGenerico utenteGenerico = new UtenteGenerico();
+        utenteGenerico.setId(1L);
+        utenteGenerico.setEmail("lab_lecce@labservice.it");
+        utenteGenerico.setPassword("prova");
+        utenteGenerico.setRole("LABORATORIO");
+
+        log.info("Creazione token di prova");
+        ConfirmationToken token = new ConfirmationToken();
+        token.setId(1L);
+        token.setToken("prova");
+        token.setExpiresAt(LocalDateTime.of(2021,11,5,18,30));
+        token.setUtenteGenerico(utenteGenerico);
+
+        log.info("Mocking token");
+        Mockito.when(confirmationTokenRepository.getById(token.getId())).thenReturn(token);
+
+        int result = confirmationTokenService.setConfirmedAt(token.getToken());
+
+
+        assertFalse(result == 1);
+
     }
 }
