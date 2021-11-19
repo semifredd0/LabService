93b2be33fb031fbcec9641beaf34eb0ae83115e6
domenico.degaetano <domenico.degaetano@finconsgroup.com>
2021-11-19 15:05:40 +0100

    Classe di test  "RegistrationServiceTest" creata

--- src/main/java/com/uniba/di/dfmdevelop/labservice/service/PaymentService.java
+++ src/main/java/com/uniba/di/dfmdevelop/labservice/service/PaymentService.java
@@ -29,7 +29,7 @@ public class PaymentService {
         Amount amount = new Amount();
         amount.setCurrency(currency);
         total = new BigDecimal(total).setScale(2, RoundingMode.HALF_UP).doubleValue();
-        amount.setTotal(String.format("%.2f", total));
+        amount.setTotal(String.format("%.3f", total));
 
         Transaction transaction = new Transaction();
         transaction.setDescription(description);
---
+++ src/test/java/com/uniba/di/dfmdevelop/labservice/service/PaymentServiceTest.java
@@ -0,0 +1,62 @@
+package com.uniba.di.dfmdevelop.labservice.service;
+
+import com.paypal.api.payments.Payment;
+import com.paypal.base.rest.PayPalRESTException;
+import com.uniba.di.dfmdevelop.labservice.LabServiceApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LabServiceApplication.class)
+public class PaymentServiceTest {
+
+    @Autowired
+    PaymentService paymentService;
+
+    @Test
+    public void creatingPayment() throws PayPalRESTException {
+
+        Payment newPayment = paymentService.createPayment(12.50,
+                                                    "EUR",
+                                                    "PAYPAL",
+                                                      "SALE",
+                                                    null,
+                                                    "http://localhost:8443/pay/cancel",
+                                                    "http://localhost:8443/pay/success");
+
+        assertTrue(newPayment != null);
+    }
+
+    /*@Test
+    public void executionOfAPayment() throws PayPalRESTException {
+
+        PayerInfo payerInfo = new PayerInfo();
+        payerInfo.setPayerId("TEST");
+        payerInfo.setBirthDate("25/12/2000");
+        payerInfo.setEmail("lab_lecce@labservice.it");
+
+        Payer payer = new Payer();
+        payer.setPayerInfo(payerInfo);
+
+        Payment newPayment = paymentService.createPayment(12.50,
+                "EUR",
+                "PAYPAL",
+                "SALE",
+                null,
+                "http://localhost:8443/pay/cancel",
+                "http://localhost:8443/pay/success");
+
+        newPayment.setState("AUTHORIZED");
+
+        Payment paymentExecute = paymentService.executePayment(newPayment.getId(), payerInfo.getPayerId());
+
+        assertTrue(paymentExecute != null);
+    }*/
+}
