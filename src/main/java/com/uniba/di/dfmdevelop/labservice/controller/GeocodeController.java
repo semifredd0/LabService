3a9324904d6dbb4f4cadaad8c77c7c93585a6ceb
semifredd0 <matteo.costantini00@gmail.com>
2021-11-27 14:40:22 +0100

    Aggiunte le classi per la geolocalizzazione con Google Maps API. Aggiornato il file pom.xml.

---
+++ src/main/java/com/uniba/di/dfmdevelop/labservice/controller/GeocodeController.java
@@ -0,0 +1,35 @@
+package com.uniba.di.dfmdevelop.labservice.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uniba.di.dfmdevelop.labservice.maps.GeocodeResult;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+
+@RestController
+public class GeocodeController {
+
+    @RequestMapping(path = "/geocode", method = RequestMethod.GET )
+    public GeocodeResult getGeocode(@RequestParam String address) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        String encodedAddress = URLEncoder.encode(address, "UTF-8");
+        Request request = new Request.Builder()
+                .url("https://google-maps-geocoding.p.rapidapi.com/geocode/json?language=en&address=" + encodedAddress)
+                .get()
+                .addHeader("x-rapidapi-host", "google-maps-geocoding.p.rapidapi.com")
+                .addHeader("x-rapidapi-key", "insert-the-secret-key")
+                .build();
+
+        ResponseBody responseBody = client.newCall(request).execute().body();
+        ObjectMapper objectMapper = new ObjectMapper();
+        GeocodeResult result = objectMapper.readValue(responseBody.string(), GeocodeResult.class);
+        return result;
+    }
+}
